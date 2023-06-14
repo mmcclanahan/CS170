a8301edb3448bafc4a1337aaf4b348d1f04e3fc4
Michael McClanahan <mmmcclan365@gmail.com>
2023-06-13 23:34:57 -0700

    Add files via upload

---
+++ MathPrinter.java
@@ -0,0 +1,14 @@
+/**
+ * Program prints the sum and product of 1,3,5,7,9
+ * 
+ * @author Michael McClanahan
+ * @version June 13,2023
+ */
+public class MathPrinter
+{
+    public static void main(String[] args)
+    {
+        System.out.println(1 + 3 + 5 + 7 + 9);
+        System.out.println(1*3*5*7*8);
+    }
+}
